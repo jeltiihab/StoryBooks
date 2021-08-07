a4228ebbd9f94b4489f9c6dc8a01bd9c8ea24d8f
jeltiihab <ihab.jelti@gmail.com>
2021-08-07 17:03:26 +0200

    Add Stories with ckEditor

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const path = require('path')
 const express = require('express')
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
 const passport = require('passport')
-const connectDB = require('./config/db')
 const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const connectDB = require('./config/db')
 
 // Load config
 dotenv.config({ path: './config/config.env' })
@@ -31,6 +33,7 @@ app.use(
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
   }),
 )
 
@@ -44,6 +47,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 // Routes
 app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
 
 const PORT = process.env.PORT || 3000
 
