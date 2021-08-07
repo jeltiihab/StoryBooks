0f3972681b7a8fd43ada40c42f5bf43446486b60
jeltiihab <ihab.jelti@gmail.com>
2021-08-07 15:07:56 +0200

    Adding user to DB

--- config/passport.js
+++ config/passport.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const User = require('../models/User')
 
 module.exports = function (passport) {
-  passport.User(
+  passport.use(
     new GoogleStrategy(
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
@@ -12,16 +12,33 @@ module.exports = function (passport) {
       },
 
       async (accessToken, refrechToken, profile, done) => {
-        console.log(profile)
+        const newUser = {
+          googleId: profile.id,
+          displayName: profile.displayName,
+          firstName: profile.name.givenName,
+          lastName: profile.name.familyName,
+          image: profile.photos[0].value,
+        }
+
+        try {
+          let user = await User.findOne({ googleId: profile.id })
+
+          if (user) {
+            done(null, user)
+          } else {
+            user = await User.create(newUser)
+            done(null, user)
+          }
+        } catch (err) {
+          console.error(err)
+        }
       },
     ),
   )
 
-  passport.serializeUser((user, done) 
-  => done(null, user.id))
+  passport.serializeUser((user, done) => done(null, user.id))
 
   passport.deserializeUser((id, done) => {
-    User.findById(id, (err, user) 
-    => done(err, user))
+    User.findById(id, (err, user) => done(err, user))
   })
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,19 @@
 const express = require('express')
+const passport = require('passport')
 const router = express.Router()
 
 // @desc    Auth with Google
 // @route   GET /auth/google
-router.get('/google', passport.authenticate('google', {}))
+router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
 
-// @desc Dashboard
-// @route GET /dashboard
-router.get('/dashboard', (req, res) => {
-  res.render('dashboard')
-})
+// @desc Google callback
+// @route GET /auth/google/callback
+router.get(
+  '/google/callback',
+  passport.authenticate('google', { failureRedirect: '/' }),
+  (req, res) => {
+    res.redirect('/dashboard')
+  },
+)
 
 module.exports = router
