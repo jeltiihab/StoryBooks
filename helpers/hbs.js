202c6f760fffd903cbbc31f29790ee847aec965a
jeltiihab <ihab.jelti@gmail.com>
2021-08-07 18:03:01 +0200

    Adding truncate and strigTags functions

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -4,4 +4,19 @@ module.exports = {
   formatDate: function (date, format) {
     return moment(date).format(format)
   },
+  // This truncate method help to get a specify length froma string given
+  truncate: function (str, len) {
+    if (str.length > len && str.length > 0) {
+      let new_str = str + ' '
+      new_str = str.substr(0, len)
+      new_str = str.substr(0, new_str.lastIndexOf(' '))
+      new_str = new_str.length > 0 ? new_str : str.substr(0, len)
+      return new_str + '...'
+    }
+    return str
+  },
+  // This function replace all given caracters with a space
+  stripTags: function (input) {
+    return input.replace(/<(?:.|\n)*?>/gm, '')
+  },
 }
--- routes/stories.js
+++ routes/stories.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const { ensureAuth } = require('../middleware/auth')
 
 const Story = require('../models/story')
+
 // @desc    Show add page
 // @route   GET /stories/add
 router.get('/add', ensureAuth, (req, res) => {
@@ -16,6 +17,22 @@ router.post('/', ensureAuth, async (req, res) => {
     req.body.user = req.user.id
     await Story.create(req.body)
     res.redirect('/dashboard')
+  } catch (err) {
+    console.error(err)
+    res.render('error/500')
+  }
+})
+
+// @desc    Show all stories
+// @route   GET /stories
+router.get('/', ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({ status: 'public' })
+      .populate('user')
+      .sort({ createdAt: 'desc' })
+      .lean()
+
+    res.render('stories/index', { stories })
   } catch (err) {
     console.error(err)
     res.render('error/500')
